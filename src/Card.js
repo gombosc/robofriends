a3794192b3c8b156e877f8f0da54df8ba3ec241c
gombosc <cosmingombos@gmail.com>
2020-11-05 00:12:21 +0200

    need sleep

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email, id}) => {
     return (
-        <div className='bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src='https://robohash.org/test?set=set2' alt='robo friend'/>
+        <div className='tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${id}?set=set2`} alt='robo friend'/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
          
